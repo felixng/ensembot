941712155c393921cee824d0271b0b6a4ecbfd66
felix.ng <nfelix@gmail.com>
2017-07-01 19:51:34 +0100

    crawling url working

--- web.js
+++ web.js
@@ -4,68 +4,75 @@ var express = require("express");
 var app = express();
 var Spooky = require('spooky');
 
-// adoped from Heroku's [Getting Started][] and [Spooky][]'s sample
-// [Getting Started]: https://devcenter.heroku.com/articles/getting-started-with-nodejs
-// [Spooky]: https://github.com/WaterfallEngineering/SpookyJS
+var links = '';
 
-var spooky = new Spooky({
-        child: {
-            transport: 'http'
-        },
-        casper: {
-            logLevel: 'debug',
-            verbose: true
-        }
-    }, function (err) {
-        if (err) {
-            e = new Error('Failed to initialize SpookyJS');
-            e.details = err;
-            throw e;
-        }
+function run(rows) {
+    var spooky = new Spooky({
+            child: {
+                transport: 'http'
+            },
+            casper: {
+                logLevel: 'debug',
+                verbose: true
+            }
+        }, function (err) {
+            if (err) {
+                e = new Error('Failed to initialize SpookyJS');
+                e.details = err;
+                throw e;
+            }
 
-        spooky.start(
-            'http://en.wikipedia.org/wiki/Spooky_the_Tuff_Little_Ghost');
-        spooky.then(function () {
-            this.emit('hello', 'Hello, from ' + this.evaluate(function () {
-                return document.title;
-            }));
+            spooky.start(
+                'http://www.officiallondontheatre.co.uk/london-shows/venue/#/?rows=' + rows + '&q=&sort=title_for_sorting_sortable%20asc');
+            spooky.then(function () {
+                this.emit('hello', this.evaluate(function () {
+                    links = document.querySelectorAll('.linkedShowsContainer .searchResults a');
+                    return Array.prototype.map.call(links, function (e) {
+                        return e.getAttribute('href')
+                    });
+                }));
+            });
+            spooky.run();
         });
-        spooky.run();
+
+    spooky.on('error', function (e, stack) {
+        console.error(e);
+
+        if (stack) {
+            console.log(stack);
+        }
     });
 
-spooky.on('error', function (e, stack) {
-    console.error(e);
+    // Uncomment this block to see all of the things Casper has to say.
+    // There are a lot.
+    // He has opinions.
+    spooky.on('console', function (line) {
+        console.log(line);
+    });
 
-    if (stack) {
-        console.log(stack);
-    }
-});
+    spooky.on('hello', function (input) {
+        console.log(input);
+        links = input;
+    });
 
+    spooky.on('log', function (log) {
+        if (log.space === 'remote') {
+            console.log(log.message.replace(/ \- .*/, ''));
+        }
+    });
 
-// Uncomment this block to see all of the things Casper has to say.
-// There are a lot.
-// He has opinions.
-spooky.on('console', function (line) {
-    console.log(line);
-});
+}
 
-var gGreeting = 'Hello World';
-
-spooky.on('hello', function (greeting) {
-    console.log(greeting);
-    gGreeting = greeting;
-});
-
-spooky.on('log', function (log) {
-    if (log.space === 'remote') {
-        console.log(log.message.replace(/ \- .*/, ''));
-    }
-});
-
+run(200);
 
 app.use(express.logger());
 app.get('/', function(request, response) {
-    response.send(gGreeting);
+    response.send(links);
+});
+
+app.get('/:rows', function(request, response) {
+    run(request.params.rows);
+    response.send();
 });
 
 var port = process.env.PORT || 5000;
